63dd08cd97d89362b72aa28bb1014e42f89b56f3
Surya170 <suryanarayana13271@gmail.com>
2022-02-28 23:39:57 +0530

    Service interface

--- BootDataJPAProj02-PagingAndSortingRepo/src/main/java/com/nt/service/ICoronaVaccineMgmtService.java
+++ BootDataJPAProj02-PagingAndSortingRepo/src/main/java/com/nt/service/ICoronaVaccineMgmtService.java
@@ -7,5 +7,5 @@ import com.nt.entity.CoronaVaccine;
 
 public interface ICoronaVaccineMgmtService {
 	public Iterable<CoronaVaccine> fetchDetails(boolean asc, String... properties);
- 
+   public Iterable<CoronaVaccine> fetchDetailsByPageNo(int pageNo,int pageSize,boolean asc,String... props);
 }
