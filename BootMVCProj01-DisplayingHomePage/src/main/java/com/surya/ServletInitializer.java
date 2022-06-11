8465d98fc9c52ef1eff3db687d094ba34a5a93f0
Surya170 <suryanarayana13271@gmail.com>
2022-06-11 11:24:53 +0530

    BootMvcProj01DisplayingHomePageApplication

--- BootMVCProj01-DisplayingHomePage/src/main/java/com/surya/ServletInitializer.java
+++ BootMVCProj01-DisplayingHomePage/src/main/java/com/surya/ServletInitializer.java
@@ -1,14 +1,17 @@
 package com.surya;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-/*
+
+//@SpringBootApplication
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(BootMvcProj01DisplayingHomePageApplication.class);
 	}
+	
 
 }
-*/
+
--- BootMVCProj01-DisplayingHomePage/src/main/java/com/surya/controller/ShowHomeController.java
+++ BootMVCProj01-DisplayingHomePage/src/main/java/com/surya/controller/ShowHomeController.java
@@ -2,12 +2,13 @@ package com.surya.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class ShowHomeController {
 
-	 @RequestMapping("/welcome")
-	//@RequestMapping("/")
+	// @RequestMapping("/welcome")
+	@RequestMapping("/")
 	public String showHomePage() {
 		return "home";
 	}
