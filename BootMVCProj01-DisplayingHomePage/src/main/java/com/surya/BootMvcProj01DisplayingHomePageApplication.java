11776ff150d80dfca2eb736a8954070568d03422
Surya170 <suryanarayana13271@gmail.com>
2022-06-13 20:02:26 +0530

    DisplayingHomePageApplication

--- BootMVCProj01-DisplayingHomePage/src/main/java/com/surya/BootMvcProj01DisplayingHomePageApplication.java
+++ BootMVCProj01-DisplayingHomePage/src/main/java/com/surya/BootMvcProj01DisplayingHomePageApplication.java
@@ -5,10 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BootMvcProj01DisplayingHomePageApplication {
-/*
 	public static void main(String[] args) {
 		SpringApplication.run(BootMvcProj01DisplayingHomePageApplication.class, args);
 	} 
-*/
+
   
 }  
