938e7c558a031769b19bb3c9108f0015bb32b71d
Surya170 <suryanarayana13271@gmail.com>
2022-06-15 14:30:09 +0530

    Contrtoller Class and Handler Method With diiferent permutations and combinations

--- BootMVCProj02-WishMessageApp/src/main/java/com/surya/controller/WishMessageGeneratorController.java
+++ BootMVCProj02-WishMessageApp/src/main/java/com/surya/controller/WishMessageGeneratorController.java
@@ -1,8 +1,14 @@
 package com.surya.controller;
 
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.surya.service.IWishMessageService;
@@ -70,5 +76,47 @@ public class WishMessageGeneratorController {
 		String result  = service.generateWishMessage();
 	    model.addAttribute("wMsg", result);
 	}*/
+	/*@RequestMapping("/wish")
+	public Model generateMessage() {
+		// use service 
+		String result  = service.generateWishMessage();
+		// create model attributes 
+	    Model model = new ExtendedModelMap();
+	    model.addAttribute("wMsg", result);
+	    return model;
+	}
+	*/
+	
+	/*@RequestMapping("/wish")
+	public Map<String,Object> generateMessage() {
+		// use service 
+		String result  = service.generateWishMessage();
+		// create model attributes 
+	    Map<String,Object> map = new HashMap();
+	    map.put("wMsg",result);
+	    return map;
+	}
+	*/
+	/*@RequestMapping("/wish")
+	public void generateMessage(HttpServletResponse res) throws Exception {
+		// use service 
+		String result  = service.generateWishMessage();
+		 // getPrintWriter pointing to response object
+		PrintWriter pw = res.getWriter();
+		res.setContentType("text/html ");
+		pw.println("<b>Wish message is :: <b>"+result);
+		return;
+		
+	}*/
+	@RequestMapping("/wish")
+	public String generateMessage(HttpServletResponse res) throws Exception {
+		// use service 
+		String result  = service.generateWishMessage();
+		 // getPrintWriter pointing to response object
+		PrintWriter pw = res.getWriter();
+		res.setContentType("text/html ");
+		pw.println("<b>Wish message is :: <b>"+result);
+		return null;
+	}
 	
 }
