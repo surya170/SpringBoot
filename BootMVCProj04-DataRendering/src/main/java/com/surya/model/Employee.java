13029bb84d0795334dff1acd17ffaa547b280056
Surya170 <suryanarayana13271@gmail.com>
2022-06-16 09:54:55 +0530

    Employee.java is a  Model Class

---
+++ BootMVCProj04-DataRendering/src/main/java/com/surya/model/Employee.java
@@ -0,0 +1,17 @@
+package com.surya.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+	 
+	 private Integer eno;
+	 private String ename;
+	 private String desg;
+	 private Double salary;
+	 
+}
