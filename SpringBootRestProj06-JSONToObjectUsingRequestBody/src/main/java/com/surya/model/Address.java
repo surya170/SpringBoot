910774b3a03eb490e80dc6a1ab538dc80c987c4b
Surya170 <suryanarayana13271@gmail.com>
2022-07-14 13:46:05 +0530

    SpringBootRest06 App

---
+++ SpringBootRestProj06-JSONToObjectUsingRequestBody/src/main/java/com/surya/model/Address.java
@@ -0,0 +1,13 @@
+package com.surya.model;
+
+import lombok.Data;
+
+@Data
+public class Address {
+    
+	private String houseNo;
+	private String streetName;
+	private String location;
+	private Long pincode;
+	
+}
