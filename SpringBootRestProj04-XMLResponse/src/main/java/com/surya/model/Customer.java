2636944482c2e29755b5083c378248cdf5cfac2c
Surya170 <suryanarayana13271@gmail.com>
2022-07-14 11:32:15 +0530

    SpringBoot Rest Fourth Application

---
+++ SpringBootRestProj04-XMLResponse/src/main/java/com/surya/model/Customer.java
@@ -0,0 +1,17 @@
+package com.surya.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+	
+	private Integer cno;
+	private String cname;
+	private String cadd;
+	private Float billAmt;
+
+}
