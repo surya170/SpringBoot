38293814bd5187d2b0d233579c7b1056c57fede0
Surya170 <suryanarayana13271@gmail.com>
2022-07-21 08:40:26 +0530

    Swagger integeration happen for these....

---
+++ SpringBootRestProj12-MiniProject-SwaggerAPI/src/main/java/com/surya/SpringBootRestProj10MiniProject01CurdOperationsApplication.java
@@ -0,0 +1,13 @@
+package com.surya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestProj10MiniProject01CurdOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestProj10MiniProject01CurdOperationsApplication.class, args);
+	}
+
+}
