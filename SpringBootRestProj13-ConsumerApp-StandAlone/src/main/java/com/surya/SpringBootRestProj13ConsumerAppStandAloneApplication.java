932f873af5e3adfa96ce01c3403eb8f4a30e11cd
Surya170 <suryanarayana13271@gmail.com>
2022-07-26 10:57:10 +0530

    Consumer Standalone App

---
+++ SpringBootRestProj13-ConsumerApp-StandAlone/src/main/java/com/surya/SpringBootRestProj13ConsumerAppStandAloneApplication.java
@@ -0,0 +1,13 @@
+package com.surya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestProj13ConsumerAppStandAloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestProj13ConsumerAppStandAloneApplication.class, args);
+	}
+
+}
